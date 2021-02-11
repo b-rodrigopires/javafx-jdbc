ffb1dd5c70b52bfeb39caaf63e25348300e6d159
Rodrigo Pires <b.rodrigopires@gmail.com>
2021-02-11 11:51:05 -0300

    DeparmentFormController

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -92,7 +92,9 @@ public class DepartmentListController implements Initializable {
 			dialogStage.showAndWait();
 		}
 		catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+			
 		}
 	}
 
